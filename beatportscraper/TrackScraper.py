3375cf3e54c8fe3c0af297a584a7ad48f6eb104f
大野木俊樹 <toshiki@LifeIsGood.local>
2018-02-27 16:10:48 +0900

    Initial: trackscraper.py

---
+++ beatportscraper/TrackScraper.py
@@ -0,0 +1,127 @@
+import requests, json
+
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
+class TrackScraper:
+
+  def __init__(self, url):
+    # Ready for getting beatport data
+    self.data = {
+      "url": url,
+    } # For data
+    ua = UserAgent()
+    headers = {
+      "User-Agent": str(ua.random),
+    }
+    html = requests.get(url, headers)
+    self.soup = BeautifulSoup(html.text, "lxml")
+    return None
+  
+
+  def find_json(self):
+    # Find and transport to json
+    t = self.soup.find_all("script")
+    t = t[5].string
+    t = t.replace("\n     window.ProductDetail = ", "")
+    self.bp_json = json.loads(t)
+    return None
+
+
+  def get_id(self):
+    # Beatport track id
+    self.data["id"] = self.bp_json["id"]
+    return None
+
+
+  def get_title(self):
+    # Track title
+    self.data["title"] = self.bp_json["title"]
+    return None
+
+  
+  def get_mix(self):
+    # Track mix
+    self.data["mix"] = self.bp_json["mix"]
+    return None
+
+
+  def get_artists(self):
+    # Artists
+    self.data["artists"] = []
+    for p in self.bp_json["artists"]:
+      self.data["artists"].append(p["name"])
+    return None
+
+
+  def get_bpm(self):
+    # BPM
+    self.data["bpm"] = self.bp_json["bpm"]
+    return None
+
+
+  def get_key(self):
+    # Transport binary text to normal text
+    t = (self.bp_json["key"]).encode()
+    if b"\xe2\x99\xaf" in t:
+      t = t.replace(b"\xe2\x99\xaf", b"#") # "#"
+    elif b"\xe2\x99\xad" in t:
+      t = t.replace(b"\xe2\x99\xad", b"b") # "b"
+    self.data["key"] = t.decode()
+    return None
+
+
+  def get_length(self):
+    # Track length
+    t = self.bp_json["duration"]["milliseconds"]
+    t = int(t / 1000)
+    self.data["length"] = t
+    return None
+
+  
+  def get_label(self):
+    # label
+    self.data["label"] = self.bp_json["label"]["name"]
+    return None
+
+
+  def get_date(self):
+    # Release date
+    self.data["date"] = self.bp_json["date"]["released"]
+    return None
+
+
+  def get_artwork(self):
+    # Track artwork
+    self.data["artwork"] = self.bp_json["images"]["large"]["url"]
+    return None
+
+
+  def get_recommendation(self):
+    # Recommendation track list
+    t = self.soup.select("#data-objects")
+    t = t[0].string
+    t = t.split(";")
+    t = t[1]
+    t = t.replace("         window.Playables = ", "")
+    t = json.loads(t)
+    rec_id = []
+    for i in range(len(t['tracks'])):
+      rec_id.append(t['tracks'][i]['id'])
+    self.data['recommendation'] = rec_id
+
+
+  def run(self):
+    # Complete data all you need
+    self.find_json()
+    self.get_title()
+    self.get_mix()
+    self.get_artists()
+    self.get_bpm()
+    self.get_key()
+    self.get_length()
+    self.get_label()
+    self.get_date()
+    self.get_artwork()
+    self.get_recommendation()
