58a51c1a83a06e3ccf8cdbe1d28f7cbd72974359
大野木俊樹 <toshiki@LifeIsGood.local>
2018-02-27 15:18:39 +0900

    update: base functions

---
+++ beatportscraper/BeatportScraper.py
@@ -0,0 +1,107 @@
+import sys
+import requests, json
+
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
+class BeatportScraper:
+
+  def __init__(self, url):
+    # Ready for getting beatport data
+    self.content = {
+      "url": url,
+    } # For data
+    ua = UserAgent()
+    headers = {
+      "User-Agent": str(ua.random),
+    }
+    html = requests.get(url, headers)
+    self.soup = BeautifulSoup(html.text, "lxml")
+    return None
+  
+
+  def find_json(self):
+    # Find and transport to json
+    t = self.soup.find_all("script")
+    t = t[5].string
+    t = t.replace("\n     window.ProductDetail = ", "")
+    self.bp_json = json.loads(t)
+    return None
+
+
+  def get_title(self):
+    # Track title
+    self.content["title"] = self.bp_json["title"]
+    return None
+
+  
+  def get_mix(self):
+    # Track mix
+    self.content["mix"] = self.bp_json["mix"]
+    return None
+
+
+  def get_artists(self):
+    # Artists
+    self.content["artists"] = []
+    for p in self.bp_json["artists"]:
+      self.content["artists"].append(p["name"])
+    return None
+
+
+  def get_bpm(self):
+    # BPM
+    self.content["bpm"] = self.bp_json["bpm"]
+    return None
+
+
+  def get_key(self):
+    # Transport binary text to normal text
+    t = (self.bp_json["key"]).encode()
+    if b"\xe2\x99\xaf" in t:
+      t = t.replace(b"\xe2\x99\xaf", b"#") # "#"
+    elif b"\xe2\x99\xad" in t:
+      t = t.replace(b"\xe2\x99\xad", b"b") # "b"
+    self.content["key"] = t.decode()
+    return None
+
+
+  def get_length(self):
+    # Track length
+    t = self.bp_json["duration"]["milliseconds"]
+    t = int(t / 1000)
+    self.content["length"] = t
+    return None
+
+  
+  def get_label(self):
+    # label
+    self.content["label"] = self.bp_json["label"]["name"]
+    return None
+
+
+  def get_date(self):
+    # Release date
+    self.content["date"] = self.bp_json["date"]["released"]
+    return None
+
+
+  def get_artwork(self):
+    # Track artwork
+    self.content["artwork"] = self.bp_json["images"]["large"]["url"]
+    return None
+
+
+  def run(self):
+    # Complete data all you need
+    self.find_json()
+    self.get_title()
+    self.get_mix()
+    self.get_artists()
+    self.get_bpm()
+    self.get_key()
+    self.get_length()
+    self.get_label()
+    self.get_date()
+    self.get_artwork()
